6cc52e5bf7b96fb6a7b618ee3156a6a10cdfdacf
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-08-26 22:39:09 +0200

    make build

---
+++ index.e05f7cc3.js
@@ -0,0 +1,2 @@
+!function e(t,n){var r=document.createElement("ul");Object.keys(n).forEach(function(t){var a=document.createElement("li");0===Object.keys(n[t]).length?a.textContent=t:(a.textContent=t,e(a,n[t])),r.append(a)}),t.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.e05f7cc3.js.map
